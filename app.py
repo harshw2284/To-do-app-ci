5a262d8ec30c6b36ab9cfe618de120f031812ba2
harshw2284 <harsbhagat0079@gmail.com>
2026-09-02 23:26:00 +0530

    fixing security issues

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def delete_todo(todo_id):
 
 
 if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host=host, port=port, debug=False)  # nosec B104
