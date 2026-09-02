c88f19ba1b0542b815b36046e85ee8ddcad8c213
harshw2284 <harsbhagat0079@gmail.com>
2026-09-02 23:40:01 +0530

    adding changes to source code

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def delete_todo(todo_id):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     host = os.environ.get('HOST', '127.0.0.1')
-    app.run(host=host, port=port, debug=False) # nosec B104
+    app.run(host=host, port=port, debug=False)  # nosec B104
